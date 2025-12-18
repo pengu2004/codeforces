820fc3b021bf5c4e830124fab4c1d5d14ec141c8
pengu2004 <tejusria02@gmail.com>
2025-12-18 23:26:43 +0530

    Movie Festivals

---
+++ Movie_Festival.py
@@ -0,0 +1,18 @@
+n = int(input())
+movies = []
+
+for _ in range(n):
+    s, e = map(int, input().split())
+    movies.append((e, s))
+
+movies.sort()  # sort by end time
+
+count = 0
+last_end = 0
+
+for e, s in movies:
+    if s >= last_end:
+        count += 1
+        last_end = e
+
+print(count)
